58b0d1cdb642865cf1c5e9ace00cdcb693c2e111
matruongne <matruong052003@gmail.com>
2025-05-27 21:44:08 +0700

    committed

--- src/v1/configs/databases/init.redis.js
+++ src/v1/configs/databases/init.redis.js
@@ -1,7 +1,7 @@
 const { createClient } = require('redis')
 const redisClient = createClient({
 	url:
-		'redis-12831.c278.us-east-1-4.ec2.redns.redis-cloud.com:12831' ||
+		'redis://default:wWfyguHIlndgQ68somHkRVBVjNwsH4bR@redis-12831.c278.us-east-1-4.ec2.redns.redis-cloud.com:12831' ||
 		process.env.REDIS_URL ||
 		process.env.REDIS_CLOUD_URL,
 	legacyMode: true,
